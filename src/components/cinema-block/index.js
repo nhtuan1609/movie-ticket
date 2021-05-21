eeec37531e34ff19da557196e5f652acfac89747
Mason <nhtuan1609@gmail.com>
2021-05-21 19:41:50 +0700

    Render movie session for cinema-block

--- src/components/cinema-block/index.js
+++ src/components/cinema-block/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import dateFormat from 'date-format';
+import moment from 'moment';
 
 import { companyList } from './CompanyList.json';
 
@@ -162,6 +164,93 @@ const useStyles = makeStyles((theme) => ({
     color: theme.palette.textColor.main,
     textAlign: 'center',
   },
+  movieItemContainer: {
+    margin: '10px',
+    padding: '10px',
+    borderRadius: '4px',
+    border: `1px solid ${theme.palette.borderColor.light}`,
+  },
+  movieItemInfor: {
+    display: 'flex',
+  },
+  movieItemImage: {
+    width: '50px',
+    height: '70px',
+    borderRadius: '4px',
+  },
+  movieItemDetail: {
+    marginLeft: '10px',
+    width: '440px',
+  },
+  movieItemName: {
+    color: theme.palette.textColor.main,
+    fontSize: '18px',
+    fontWeight: '500',
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+  },
+  movieItemLength: {
+    color: theme.palette.textColor.light,
+    fontSize: '16px',
+    fontWeight: '400',
+  },
+  movieAgeTypeAll: {
+    minWidth: '34px',
+    lineHeight: '22px',
+    textAlign: 'center',
+    backgroundColor: 'rgb(0,172,77)',
+    color: 'white',
+    fontSize: '16px',
+    fontWeight: '600',
+    padding: '0 4px',
+    borderRadius: '4px',
+    marginRight: '6px',
+    height: '24px',
+    float: 'left',
+  },
+  movieAgeTypeSpecial: {
+    minWidth: '34px',
+    lineHeight: '22px',
+    textAlign: 'center',
+    backgroundColor: theme.palette.primary.main,
+    color: 'white',
+    fontSize: '16px',
+    fontWeight: '600',
+    padding: '0 4px',
+    borderRadius: '4px',
+    marginRight: '6px',
+    height: '24px',
+    float: 'left',
+  },
+  movieItemSession: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  sessionContainer: {
+    width: '110px',
+    padding: '2px 0px',
+    borderRadius: '4px',
+    border: `1px solid ${theme.palette.borderColor.light}`,
+    textDecoration: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: '10px',
+    marginRight: '10px',
+    '&:hover': {
+      border: `1px solid ${theme.palette.primary.main}`,
+    },
+  },
+  sessionStart: {
+    color: theme.palette.primary.main,
+    fontSize: '18px',
+    fontWeight: '500',
+  },
+  sessionEnd: {
+    color: theme.palette.textColor.light,
+    fontSize: '14px',
+    fontWeight: '400',
+  },
   horizontalSeparate: {
     width: '100%',
     borderBottom: `1px solid ${theme.palette.borderColor.light}`,
@@ -180,6 +269,20 @@ export default function CinemaBlock() {
 
   const handleSelectCinema = (cinemaCode) => (event) => {
     setCurrentCinemaCode(cinemaCode);
+  };
+
+  const renderMovieType = () => {
+    const ageAllow = Math.floor(Math.random() * 4);
+    switch (ageAllow) {
+      case 1:
+        return <span className={classes.movieAgeTypeSpecial}>C13</span>;
+      case 2:
+        return <span className={classes.movieAgeTypeSpecial}>C16</span>;
+      case 3:
+        return <span className={classes.movieAgeTypeSpecial}>C18</span>;
+      default:
+        return <span className={classes.movieAgeTypeAll}>P</span>;
+    }
   };
 
   const renderCompanyList = (
@@ -241,7 +344,7 @@ export default function CinemaBlock() {
               <span className={classes.cinemaItemInforName}>
                 <span className={classes.cinemaItemInforNameHighlight}>
                   {item.company}
-                </span>
+                </span>{' '}
                 - {item.location}
               </span>
               <span className={classes.cinemaItemInforAddress}>
@@ -261,6 +364,24 @@ export default function CinemaBlock() {
     });
   };
 
+  const renderSessionList = (sessionList, movieLength) => {
+    return sessionList.map((item, index) => {
+      let starDate = new Date(item.ngayChieuGioChieu);
+      let endDate = moment(starDate).add(movieLength, 'm').toDate();
+      return (
+        <a className={classes.sessionContainer} href='/' key={index}>
+          <span className={classes.sessionStart}>
+            {dateFormat('hh:mm', starDate)}
+          </span>
+          &nbsp;
+          <span className={classes.sessionEnd}>
+            ~ {dateFormat('hh:mm', endDate)}
+          </span>
+        </a>
+      );
+    });
+  };
+
   const renderMovieList = (
     companyList,
     currentCompanyCode,
@@ -274,12 +395,45 @@ export default function CinemaBlock() {
       (item) => item.code === currentCinemaCode
     );
 
-    return (
-      <p className={classes.movieListNotify}>
-        {currentCinemaItem.company} - {currentCinemaItem.location} hiện tại
-        không có suất chiếu
-      </p>
-    );
+    let movieList = currentCinemaItem.movieList;
+
+    if (movieList === undefined || movieList.length === 0) {
+      return (
+        <p className={classes.movieListNotify}>
+          {currentCinemaItem.company} - {currentCinemaItem.location} hiện tại
+          không có suất chiếu
+        </p>
+      );
+    } else {
+      return movieList.map((item, index) => {
+        let movieLength = Math.floor(Math.random() * 20 + 90);
+        let movieIMDb = (Math.floor(Math.random() * 13) + 8) / 2;
+        return (
+          <div className={classes.movieItemContainer} key={index}>
+            <div className={classes.movieItemInfor}>
+              <img
+                className={classes.movieItemImage}
+                src={item.hinhAnh}
+                alt={item.tenPhim}
+              ></img>
+              <div className={classes.movieItemDetail}>
+                <div className={classes.movieItemName}>
+                  {renderMovieType()}
+                  {item.tenPhim}
+                </div>
+                <div className={classes.movieItemLength}>
+                  {movieLength} Phút - IMDb: {movieIMDb}
+                </div>
+              </div>
+            </div>
+            <div className={classes.movieItemSession}>
+              {' '}
+              {renderSessionList(item.lstLichChieuTheoPhim, movieLength)}
+            </div>
+          </div>
+        );
+      });
+    }
   };
 
   return (
